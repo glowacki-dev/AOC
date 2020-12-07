f5a50dda39859881e01415be71781c523f17e24c
Maciej Głowacki <bombasarkadian@gmail.com>
2020-12-07 08:07:45 +0100

    Day06 solution

--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -6,7 +6,7 @@ namespace AOC2020
     {
         public static void Main(string[] args)
         {
-            new Runner<string>(DataType.real, new Day05()).Run();
+            new Runner<string>(DataType.real, new Day06()).Run();
         }
     }
 }
---
+++ AOC2020/Solutions/Day06.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2020
+{
+    internal class Day06 : ISolution<string>
+    {
+        public object Run(Input<string> lines)
+        {
+            int groupAnswersCount = 0;
+            string groupAnswers = "";
+            int totalPositive = 0;
+            foreach(string answers in lines.RawLines)
+            {
+                if(answers == "")
+                {
+                    totalPositive += groupAnswers.GroupBy(answer => answer).Where(group => group.Count() == groupAnswersCount).Count();
+                    groupAnswers = "";
+                    groupAnswersCount = 0;
+                }
+                else
+                {
+                    groupAnswersCount++;
+                    groupAnswers += answers;
+                }
+            }
+            totalPositive += groupAnswers.GroupBy(answer => answer).Where(group => group.Count() == groupAnswersCount).Count();
+            return totalPositive;
+        }
+    }
+}
