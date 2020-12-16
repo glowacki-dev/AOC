be42aad4e67df6f9093f74a7b1e30f4f2d5109b5
Maciej Głowacki <bombasarkadian@gmail.com>
2020-12-16 09:31:49 +0100

    Day15 solution

--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -6,7 +6,7 @@ namespace AOC2020
     {
         public static void Main(string[] args)
         {
-            new Runner<string>(DataType.real, new Day14()).Run();
+            new Runner<string>(DataType.real, new Day15()).Run();
         }
     }
 }
---
+++ AOC2020/Solutions/Day15.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2020
+{
+    internal class Day15 : ISolution<string>
+    {
+        public object Run(Input<string> lines)
+        {
+            List<int> input = lines.Lines.First().Split(',').Select(i => int.Parse(i)).ToList();
+            int turnNumber = 0;
+            Dictionary<int, int> lastOccurence = new Dictionary<int, int>();
+            Dictionary<int, int> turnsApart = new Dictionary<int, int>();
+            int lastNumber = 0;
+            foreach(int startingNumber in input)
+            {
+                lastOccurence[startingNumber] = turnNumber;
+                lastNumber = startingNumber;
+                turnNumber++;
+            }
+            while(turnNumber < 30000000)
+            {
+                if(turnsApart.ContainsKey(lastNumber))
+                {
+                    lastNumber = turnsApart[lastNumber];
+                }
+                else
+                {
+                    lastNumber = 0;
+                }
+
+                if (lastOccurence.ContainsKey(lastNumber))
+                {
+                    turnsApart[lastNumber] = turnNumber - lastOccurence[lastNumber];
+                }
+                lastOccurence[lastNumber] = turnNumber;
+                turnNumber++;
+            }
+            return lastNumber;
+        }
+    }
+}
