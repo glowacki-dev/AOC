ea29cc1e22f011233a55c06c24efe17c01dcd331
Maciej Głowacki <2972266+glowacki-dev@users.noreply.github.com>
2022-12-04 08:04:13 +0100

    Day 03

--- AOC2022/solutions/03.js
+++ AOC2022/solutions/03.js
@@ -4,21 +4,30 @@ class Solver {
   constructor(data) {
     this.data = [];
     data.forEach((line) => {
-      if (line !== "") this.data.push(_.chunk(line.split(""), line.length / 2));
+      if (line !== "") this.data.push(line.split(""));
     });
   }
 
   run() {
     var score = 0;
-    this.data.forEach(([left, right]) => {
-      let common = _.intersection(left, right);
-      common
-        .map((letter) => {
-          if (letter >= "A" && letter <= "Z")
-            return letter.charCodeAt(0) - "A".charCodeAt(0) + 27;
-          else return letter.charCodeAt(0) - "a".charCodeAt(0) + 1;
-        })
-        .forEach((s) => (score += s));
+    var counter = 0;
+    var arrays = [];
+    this.data.forEach((arr) => {
+      counter++;
+      arrays.push(arr);
+      if (counter === 3) {
+        let common = _.intersection(...arrays);
+        if (common.length !== 1) console.error("Something is wrong");
+        common
+          .map((letter) => {
+            if (letter >= "A" && letter <= "Z")
+              return letter.charCodeAt(0) - "A".charCodeAt(0) + 27;
+            else return letter.charCodeAt(0) - "a".charCodeAt(0) + 1;
+          })
+          .forEach((s) => (score += s));
+        counter = 0;
+        arrays = [];
+      }
     });
     return score;
   }
